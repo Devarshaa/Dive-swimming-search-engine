7835ddbbe13659ba18dc8af588e8c6b0e7c8de0d
Devarshaa Sikharam <57261370+Devarshaa@users.noreply.github.com>
2023-04-29 09:36:52 -0500

    Add files via upload
    Query Expansion

---
+++ QueryExpansion/MetricClusters.py
@@ -0,0 +1,52 @@
+from util import tokenize_and_stem
+from collections import defaultdict
+# from index.solr_client import search
+
+def find_indices(list_to_check, item_to_find):
+    return [idx for idx, value in enumerate(list_to_check) if value == item_to_find]
+
+def findMostCorrelated(tokens, queryTerms, doc_dict):
+    metrics = defaultdict(int)
+    for term in set(queryTerms):
+        for stem in set(tokens):
+            c = 0
+            for doc in doc_dict.values():
+                term_count = doc.count(term)
+                stem_count = doc.count(stem)
+                if term_count == 0 or stem_count == 0:
+                    continue
+                term_list = find_indices(doc, term)
+                stem_list = find_indices(doc, stem)
+                for idx1 in term_list:
+                    for idx2 in stem_list:
+                        if idx1 != idx2:
+                            c += (1/abs(idx1 - idx2))
+            c /= (tokens.count(term) * tokens.count(stem))
+            metrics[(term, stem)] = c
+    return metrics
+
+
+def expandQueryMC(query, resultSet):
+    tokens = []
+    doc_dict = {}
+    queryStems = tokenize_and_stem(query)
+    for result in resultSet:
+        doc_tokens = tokenize_and_stem(result['meta_info'])
+        doc_dict[result['url'][0]] = doc_tokens
+        tokens.extend(doc_tokens)
+
+    # localVocab = set(tokens)
+    metrics = findMostCorrelated(tokens, queryStems, doc_dict)
+    metrics = sorted(metrics.items(), key=lambda item: item[1], reverse=True)[:5]
+    for item in metrics:
+        if item[0][1] not in queryStems:
+            query += ' ' +item[0][1]
+            queryStems.append(item[0][1])
+    return query
+
+# docs = search('olympics medals', 30)
+# print(docs[0])
+# new = expandQueryMC('olympics medals', docs)
+# print(new)
+# docs1 = search(new, 30)
+# print(docs1[0])
---
+++ QueryExpansion/PseudoRelevanceFeedback.py
@@ -0,0 +1,33 @@
+# Rocchio Algorithm for Pseudo-Relevance Feedback
+from util import tokenize_and_stem
+from collections import defaultdict
+# from index.solr_client import search
+
+def expandQuery(query, resultSet):
+    beta = 0.6
+    gamma = 0.2
+    queryStems = tokenize_and_stem(query)
+    query_vector = defaultdict(int)
+    for stem in set(queryStems):
+        query_vector[stem] = queryStems.count(stem)
+    for result in resultSet[:20]:
+        doc_tokens = tokenize_and_stem(result['meta_info'])
+        for stem in set(doc_tokens):
+            query_vector[stem] += (beta * doc_tokens.count(stem))
+    for result in resultSet[20:]:
+        doc_tokens = tokenize_and_stem(result['meta_info'])
+        for stem in set(doc_tokens):
+            query_vector[stem] -= (gamma * doc_tokens.count(stem))
+    best = sorted(query_vector.items(), key=lambda item: item[1], reverse=True)[:5]
+    for item in best:
+        if item[0] not in queryStems:
+            query += ' ' + item[0]
+
+    return query
+
+# docs = search('olympics medals', 30)
+# print(docs[0])
+# new = expandQuery('olympics medals', docs)
+# print(new)
+# docs1 = search(new, 30)
+# print(docs1[0])
